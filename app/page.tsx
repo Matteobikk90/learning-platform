ff68a38b0725f88d2abc0c8656d34abeda2509a4
Matteo Soresini <matteo.soresini@hotmail.it>
2026-04-28 18:09:47 +0200

    feat: setup prisma client and test query

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import { prisma } from "@/lib/prisma";
+
+export default async function Home() {
+  const courses = await prisma.course.findMany();
+
   return (
     <main className="p-10">
-      <h1 className="text-3xl font-bold">Learning Platform 🚀</h1>
+      <h1 className="text-3xl font-bold">Courses: {courses.length}</h1>
     </main>
   );
 }
